f78028f509f7fd3578d76d2443ff1e1027e09baa
AAmanzi <alnick.amanzi@gmail.com>
2019-05-22 08:09:13 +0200

    Slight change to Game header file

--- Project-Blackjack/Project-Blackjack/Game.h
+++ Project-Blackjack/Project-Blackjack/Game.h
@@ -20,7 +20,9 @@ public:
 	void DisplayAllCards();
 	void DealerTurn();
 	void DealerFinishUp();
-	void OpponentTurn(int choice); //1 meaning drawing a card, 0 skipping
+	void OpponentTurn(int choice); 
+	//int choice: 
+	//	1 meaning drawing a card, all else skipping
 	bool IsGameOver() const;
 	std::string GetWinner();
 };
